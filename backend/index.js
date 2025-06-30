afe3c63159a5dae22e00a4b2c49d741cd816fc13
Ajay kumar harijan <ajaykumar895377@gmail.com>
2025-06-30 18:29:21 +0530

    update cors origin

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ db();
   
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:3000",'https://resilient-dusk-feca6c.netlify.app'], // or replace with actual frontend origin
+  origin: ["http://localhost:3000",'https://noteleaf.netlify.app/'], // or replace with actual frontend origin
   credentials: true,
 }));
 
